b7a5af879cb9126f77e5b7beca793c1bffc2b292
Henrique Aguiar <henriqueaguiarpacheco09@gmail.com>
2025-01-21 18:17:00 -0300

    get gameById

--- src/main/java/com/henriqueaguiiar/dslist/services/GameService.java
+++ src/main/java/com/henriqueaguiiar/dslist/services/GameService.java
@@ -1,6 +1,7 @@
 package com.henriqueaguiiar.dslist.services;
 
 
+import com.henriqueaguiiar.dslist.dto.GameDTO;
 import com.henriqueaguiiar.dslist.dto.GameMinDTO;
 import com.henriqueaguiiar.dslist.entities.Game;
 import com.henriqueaguiiar.dslist.repositories.GameRepository;
@@ -15,6 +16,13 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @org.springframework.transaction.annotation.Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        Game result = gameRepository.findById(id).get();
+        GameDTO dto = new GameDTO(result);
+        return dto;
+    }
+    @org.springframework.transaction.annotation.Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> result = gameRepository.findAll();
         List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
